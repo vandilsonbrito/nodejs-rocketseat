cd41362c01967b1c7deb7b4d006b86528f96afd6
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-01-09 17:08:14 -0300

    chore: delete explanation text

--- 01-fundamentos/src/server.js
+++ 01-fundamentos/src/server.js
@@ -2,29 +2,6 @@ import http from 'http';
 import { json } from './middlewares/json.js';
 import { routes } from './routes.js';
 
-// - HTTP
-// - Método HTTP
-// - URL
-
-// GET, POST, PUT, PATCH, DELETE
-
-// GET => Buscar um recurso do back-end
-// POST => Criar um recurso no back-end
-// PUT => Alterar/atualizar um recurso no back-end
-// PATCH => Alterar/atualizar uma informação específica de um recurso no back-end
-// DELETE => Deletar um recurso no back-end
-
-// Stateful (estado em memoria) - Stateless (sem memoria de estado)
-
-// Cabeçalhos (Requisição/Resposta) => Metadados
-
-// HTTP Response Status Code
-// 100 - 199 - Informational
-// 200 - 299 - Success
-// 300 - 399 - Redirection
-// 400 - 499 - Client Error
-// 500 - 599 - Server Error
-
 const server = http.createServer(async (req, res) => {
 
     const { method, url } = req;
