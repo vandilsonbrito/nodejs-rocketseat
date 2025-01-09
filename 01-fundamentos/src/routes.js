64164af03de7661e71456a9ad6ef96478a1bf74a
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-01-09 11:44:12 -0300

    feart: create file to list all server routes and export it

---
+++ 01-fundamentos/src/routes.js
@@ -0,0 +1,33 @@
+import { Database } from './database.js';
+import { randomUUID } from 'crypto';
+
+const database = new Database();
+
+export const routes = [
+    {
+        method: 'GET',
+        path: '/users',
+        handler: async (req, res) => {
+            const users = database.select('users');
+
+            return res.end(JSON.stringify(users));
+        }
+    },
+    {
+        method: 'POST',
+        path: '/users',
+        handler: async (req, res) => {
+            const { name, email } = req.body;
+
+            const user = {
+                id: randomUUID(),
+                name,
+                email,
+            };
+
+            database.insert('users', user);
+
+            res.writeHead(201).end();
+        }   
+    }
+]
