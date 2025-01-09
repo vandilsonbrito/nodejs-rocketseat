f27df3d95da8f258a6a86ff689ce2a3d95144c49
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-01-09 10:19:10 -0300

    feat: create persist method and constructor to save and read data from json-db

--- 01-fundamentos/src/database.js
+++ 01-fundamentos/src/database.js
@@ -1,5 +1,24 @@
+import fs from 'node:fs/promises'
+
+const databasePath = new URL('db.json', import.meta.url)
+
 export class Database {
     #database = {}
+
+
+    #persist() {
+        fs.writeFile(databasePath, JSON.stringify(this.#database))
+    }
+
+    constructor() {
+        fs.readFile(databasePath, 'utf-8')
+            .then(data => {
+                this.#database = JSON.parse(data)
+            })
+            .catch(err => {
+                this.#persist()
+            })
+    }
 
     select(table) {
         const data = this.#database[table] ?? []
@@ -12,6 +31,7 @@ export class Database {
         } else {
             this.#database[table] = [data];
         }
+        this.#persist();
         return data;
     }
 }
