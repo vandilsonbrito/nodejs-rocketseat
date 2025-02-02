bbb2105d28270de3ccdf6376211e962fa31dd5fa
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-02-02 13:04:25 -0300

    feat: creates routes for crud and its logic to interact with the database

--- 01-fundamentos/src/routes.js
+++ 01-fundamentos/src/routes.js
@@ -1,42 +1,81 @@
-import { Database } from './database.js';
-import { randomUUID } from 'crypto';
+import { randomUUID } from 'node:crypto'
+import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
 
-const database = new Database();
+const database = new Database()
 
 export const routes = [
-    {
-        method: 'GET',
-        path: '/users',
-        handler: async (req, res) => {
-            const users = database.select('users');
+  {
+    method: 'GET',
+    path: buildRoutePath('/users'),
+    handler: (req, res) => {
+      const { search } = req.query
 
-            return res.end(JSON.stringify(users));
-        }
-    },
-    {
-        method: 'POST',
-        path: '/users',
-        handler: async (req, res) => {
-            const { name, email } = req.body;
+      const users = database.select('users', search ? {
+        name: search,
+        email: search
+      } : null)
 
-            const user = {
-                id: randomUUID(),
-                name,
-                email,
-            };
-
-            database.insert('users', user);
-
-            res.writeHead(201).end();
-        }   
-    },
-    {
-        method: 'DELETE',
-        path: '/users',
-        handler: async (req, res) => {
-            
-
-            res.writeHead(201).end();
-        }   
+      return res.end(JSON.stringify(users))
     }
+  },
+  {
+    method: 'POST',
+    path: buildRoutePath('/users'),
+    handler: (req, res) => {
+        if (!req.body) {
+            return res.writeHead(400).end('Corpo da requisição inválido ou ausente');
+        }
+      
+        const { name, email } = req.body;
+    
+        if (!name || !email) {
+            return res.writeHead(400).end('Campos "name" e "email" são obrigatórios');
+        }
+
+      const user = {
+        id: randomUUID(),
+        name,
+        email,
+      }
+
+      database.insert('users', user)
+
+      return res.writeHead(201).end()
+    }
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      if (!req.body) {
+        return res.writeHead(400).end('Corpo da requisição inválido ou ausente');
+     }
+  
+     const { name, email } = req.body;
+
+     if (!name || !email) {
+        return res.writeHead(400).end('Campos "name" e "email" são obrigatórios');
+     } 
+
+      database.update('users', id, {
+        name,
+        email,
+      })
+
+      return res.writeHead(204).end()
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      database.delete('users', id)
+
+      return res.writeHead(204).end()
+    }
+  }
 ]
