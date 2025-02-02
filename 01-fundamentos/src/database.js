cbba13462e5d64b734d24bfd295c4aa3f25aa4f6
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-02-02 12:59:33 -0300

    feat: creates filter on select ad adds update and delete methods on database

--- 01-fundamentos/src/database.js
+++ 01-fundamentos/src/database.js
@@ -1,37 +1,65 @@
 import fs from 'node:fs/promises'
 
-const databasePath = new URL('db.json', import.meta.url)
+const databasePath = new URL('../db.json', import.meta.url)
 
 export class Database {
-    #database = {}
+  #database = {}
 
+  constructor() {
+    fs.readFile(databasePath, 'utf8')
+      .then(data => {
+        this.#database = JSON.parse(data)
+      })
+      .catch(() => {
+        this.#persist()
+      })
+  }
 
-    #persist() {
-        fs.writeFile(databasePath, JSON.stringify(this.#database))
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
+
+  select(table, search) {
+    let data = this.#database[table] ?? []
+
+    if (search) {  
+      data = data.filter(row => {
+        return Object.entries(search).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase())
+        })
+      })
     }
 
-    constructor() {
-        fs.readFile(databasePath, 'utf-8')
-            .then(data => {
-                this.#database = JSON.parse(data)
-            })
-            .catch(err => {
-                this.#persist()
-            })
+    return data
+  }
+
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data)
+    } else {
+      this.#database[table] = [data]
     }
 
-    select(table) {
-        const data = this.#database[table] ?? []
-        return data;
-    }
+    this.#persist()
 
-    insert(table, data) {
-        if(Array.isArray(this.#database[table])) {
-            this.#database[table].push(data);
-        } else {
-            this.#database[table] = [data];
-        }
-        this.#persist();
-        return data;
+    return data
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    if (rowIndex > -1) {
+      this.#database[table][rowIndex] = { id, ...data }
+      this.#persist()
     }
+  }
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1)
+      this.#persist()
+    }
+  }
 }
