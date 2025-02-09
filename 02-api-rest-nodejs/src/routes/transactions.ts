3a4d4110108ccd8e506a5eabfd19559f44bd440e
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-02-08 21:31:22 -0300

    feat: creates transactions routes

--- 02-api-rest-nodejs/src/routes/transactions.ts
+++ 02-api-rest-nodejs/src/routes/transactions.ts
@@ -1,15 +1,52 @@
 import { FastifyInstance } from "fastify";
 import { knex } from "../database";
-import crypto from "node:crypto";
+import { randomUUID } from "node:crypto";
+import { z } from "zod";
 
 export async function transactionsRoutes(app: FastifyInstance) {
-    app.get('/test', async () => {
-        const transactions = await knex('transactions').insert({
-            id: crypto.randomUUID(),
-            title: 'Transação de Teste',
-            amount: 1000
-        }).returning('*')
-    
-        return transactions;
+
+    app.get('/', async () => {
+        const transactions = await knex('transactions').select('*')
+
+        return { transactions}
     })
+
+    app.get('/:id', async (req) => {
+        const getTransactionParamsSchema = z.object({
+            id: z.string().uuid()
+        })
+
+        const { id } = getTransactionParamsSchema.parse(req.params)
+
+        const transaction = await knex('transactions').where('id', id).first()
+
+        return { transaction }
+    })
+
+    app.get('/summary', async () => {
+        const summary = await knex('transactions').sum('amount', { as: 'amount' }).first()
+
+        return { summary }
+    })
+
+    app.post('/', async (req, res) => {
+
+        const createTransactionBodySchema = z.object({
+            title: z.string(),
+            amount: z.number(),
+            type: z.enum((['credit', 'debit']))
+        })
+
+        // verify if body params are valid
+        const { title, amount, type } = createTransactionBodySchema.parse(req.body)
+
+        await knex('transactions').insert({
+            id: randomUUID(),
+            title,
+            amount: type === 'credit' ? amount : amount * -1
+        })
+
+        return res.status(201).send('Transaction created successfully')
+    })
+
 }
