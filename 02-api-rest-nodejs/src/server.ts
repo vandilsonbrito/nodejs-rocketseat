f56b1f0ad7eaeeedac49755538fcad0be8229c06
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-02-08 21:06:27 -0300

    feat: adds route prefix to the app

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -4,10 +4,12 @@ import { transactionsRoutes } from "./routes/transactions";
 
 const app = fastify();
 
-app.register(transactionsRoutes)
+app.register(transactionsRoutes, {
+    prefix: 'transactions',
+})
 
 app.listen({
     port: env.PORT
 }).then(() => {
-    console.log(`Server is running on PORT: 5000`);
+    console.log(`Server is running on PORT: ${env.PORT}`);
 })
