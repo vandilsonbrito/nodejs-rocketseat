c9c6e3e25bcccad0df8c936d07ed14f502a589d6
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-02-09 23:01:33 -0300

    feat: ensure PORT from .env file is a number using z.preprocess

--- 02-api-rest-nodejs/src/env/index.ts
+++ 02-api-rest-nodejs/src/env/index.ts
@@ -4,7 +4,7 @@ import z from 'zod';
 const envSchema = z.object({
     NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
     DATABASE_URL: z.string(),
-    PORT: z.number().default(5000),
+    PORT: z.preprocess((val) => Number(val), z.number()).default(5000),
 })
 
 const _env = envSchema.safeParse(process.env)
