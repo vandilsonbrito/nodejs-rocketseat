b14c5668929fa481d6031dd39b022930ce63af99
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-04-03 17:38:09 -0300

    refactor: import find-by-email method and add correct type to register-repository

--- 03-api-solid/services/register.ts
+++ 03-api-solid/services/register.ts
@@ -1,5 +1,5 @@
 import { hash } from "bcryptjs"
-import { prisma } from "lib/prisma"
+import { UsersRepository } from "repositories/users-repository"
 
 interface RegisterRequest {
     name: string
@@ -9,17 +9,13 @@ interface RegisterRequest {
 
 // Dependency Inversion Principle
 export class RegisterService { 
-    constructor(private usersRepository: any) {}
+    constructor(private usersRepository: UsersRepository) {}
 
     async execute({ name, email, password }: RegisterRequest) {
         const password_hash = await hash(password, 6)
     
-        const userWithSameEmail = await prisma.user.findUnique({
-            where: {
-                email
-            }
-        })
-    
+        const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
         if (userWithSameEmail) {
             throw new Error('Email already exists')
         } 
