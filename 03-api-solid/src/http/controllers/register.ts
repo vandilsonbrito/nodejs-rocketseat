b78fd33fc5103934caf69dd74d243c6a30db31ae
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-04-03 17:39:45 -0300

    refactor: rename const that instance prisma-users-repository

--- 03-api-solid/src/http/controllers/register.ts
+++ 03-api-solid/src/http/controllers/register.ts
@@ -1,9 +1,7 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
-import { prisma } from '../../../lib/prisma'
 import { z } from 'zod'
-import { hash } from 'bcryptjs'
 import { RegisterService } from 'services/register'
-import { PrismaUsersRepository } from 'repositories/prisma-users-repository'
+import { PrismaUsersRepository } from 'repositories/prisma/prisma-users-repository'
 
 export async function register (request: FastifyRequest, reply: FastifyReply) {
 
@@ -16,10 +14,10 @@ export async function register (request: FastifyRequest, reply: FastifyReply) {
     const { name, email, password } = registeredBodySchema.parse(request.body)
 
     try {
-        const prismaUsersRepository = new PrismaUsersRepository()
         // the service who needs the dependency is the one that is going to call the repository => Dependency Inversion
         // then it would be easier to change the repository (change from Prisma to something else for example (TypeORM, MongoDB, etc))
-        const registerService = new RegisterService(prismaUsersRepository) 
+        const usersRepository = new PrismaUsersRepository()
+        const registerService = new RegisterService(usersRepository) 
 
         await registerService.execute({
             name,
