2fa0b51edde1e1db6d70b84bef44d9999e9348ec
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-04-03 16:10:25 -0300

    refactor: add register service

--- 03-api-solid/src/http/controllers/register.ts
+++ 03-api-solid/src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 import { prisma } from '../../../lib/prisma'
 import { z } from 'zod'
 import { hash } from 'bcryptjs'
+import { registerUseCase } from 'services/register'
 
 export async function register (request: FastifyRequest, reply: FastifyReply) {
 
@@ -13,25 +14,16 @@ export async function register (request: FastifyRequest, reply: FastifyReply) {
 
     const { name, email, password } = registeredBodySchema.parse(request.body)
 
-    const password_hash = await hash(password, 6)
-
-    const userWithSameEmail = await prisma.user.findUnique({
-        where: {
-            email
-        }
-    })
-
-    if (userWithSameEmail) {
-        return reply.status(409).send()
-    } 
-
-    await prisma.user.create({
-        data: {
+    try {
+        await registerUseCase({
             name,
             email,
-            password_hash
-        }
-    })
+            password
+        })
+    }
+    catch (error) {
+        return reply.status(409).send()
+    }
 
     return reply.status(201).send()
 }
