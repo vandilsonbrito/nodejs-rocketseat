5c19e841dabc38b861a3d473412dbbada69ac737
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-04-04 17:43:25 -0300

    feat: create initial test for register service: hash password

---
+++ 03-api-solid/services/register.spec.ts
@@ -0,0 +1,24 @@
+import { expect, describe, it } from 'vitest'
+import { RegisterService } from './register'
+import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from 'repositories/in-memory/in-memory-users-repository'
+
+describe('Register service', () => {
+    it('should hash user password upon registration', async () => {
+        const usersRepository = new InMemoryUsersRepository()
+        const registerService = new RegisterService(usersRepository)
+
+        const { user } = await registerService.execute({
+            name: 'John Doe',
+            email: 'email@example.com',
+            password: '123456'
+        })
+
+        const isPasswordCorrectlyHashed = await compare(
+            '123456', 
+            user.password_hash
+        )
+        console.log(isPasswordCorrectlyHashed)
+        expect(isPasswordCorrectlyHashed).toBe(true)
+    })
+})
