5a788b44b049296bf81532921c78043c3f7ca65d
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-04-03 21:54:41 -0300

    feat: add email exists error handling

--- 03-api-solid/services/register.ts
+++ 03-api-solid/services/register.ts
@@ -1,5 +1,6 @@
 import { hash } from "bcryptjs"
 import { UsersRepository } from "repositories/users-repository"
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error"
 
 interface RegisterRequest {
     name: string
@@ -17,7 +18,7 @@ export class RegisterService {
         const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
         if (userWithSameEmail) {
-            throw new Error('Email already exists')
+            throw new UserAlreadyExistsError()
         } 
         
         await this.usersRepository.create({
--- 03-api-solid/src/http/controllers/register.ts
+++ 03-api-solid/src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 import { RegisterService } from 'services/register'
 import { PrismaUsersRepository } from 'repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from 'services/errors/user-already-exists-error'
 
 export async function register (request: FastifyRequest, reply: FastifyReply) {
 
@@ -26,7 +27,10 @@ export async function register (request: FastifyRequest, reply: FastifyReply) {
         })
     }
     catch (error) {
-        return reply.status(409).send()
+        if (error instanceof UserAlreadyExistsError) {
+            return reply.status(409).send({ message: error.message })
+        }
+        return reply.status(500).send()
     }
 
     return reply.status(201).send()
