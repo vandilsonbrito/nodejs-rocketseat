60c8a63369ffbfaf296646545a112ae77ccf8332
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-04-05 21:06:40 -0300

    feat: create check-in use case

---
+++ 03-api-solid/use-cases/check-in.ts
@@ -0,0 +1,31 @@
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
+import { compare } from "bcryptjs";
+import { CheckIn } from "@prisma/client";
+import { CheckInsRepository } from "repositories/check-ins-repositories";
+
+interface CheckInUseCaseRequest {
+    userId: string
+    gymId: string
+}
+
+interface CheckInUseCaseResponse {
+    checkIn: CheckIn
+}
+
+export class CheckInUseCase {
+    constructor(
+        private checkInsRepository: CheckInsRepository
+    ) {}
+
+    async execute({ userId, gymId }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+        const checkIn = await this.checkInsRepository.create({
+            user_id: userId,
+            gym_id: gymId
+        })
+
+
+        return {
+            checkIn
+        }
+    }
+ }
