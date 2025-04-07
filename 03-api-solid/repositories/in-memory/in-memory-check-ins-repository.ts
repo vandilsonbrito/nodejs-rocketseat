637d14cd08145bdd5b81648d883910ceaf8568b2
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-04-06 22:24:33 -0300

    feat: create findByUserIdOnDate function verifying checkin on same date

--- 03-api-solid/repositories/in-memory/in-memory-check-ins-repository.ts
+++ 03-api-solid/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -4,6 +4,22 @@ import { CheckInsRepository } from "repositories/check-ins-repositories";
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
     public items: CheckIn[] = []
+
+    async findByUserIdOnDate(userId: string, date: Date) {
+        const checkInOnSameDate = this.items.find(
+            (checkIn) =>
+                checkIn.user_id === userId &&
+                checkIn.created_at.getFullYear() === date.getFullYear() &&
+                checkIn.created_at.getMonth() === date.getMonth() &&
+                checkIn.created_at.getDate() === date.getDate(),
+        )
+
+        if (!checkInOnSameDate) {
+            return null
+        }
+
+        return checkInOnSameDate
+    }
     
     async create(data: Prisma.CheckInUncheckedCreateInput) {
         const checkIn = {
