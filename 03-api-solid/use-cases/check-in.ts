ce825239227ae2fdcdd12defeb9668800052dac6
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-04-06 22:26:30 -0300

    feat: add checkInOnSameDay verification on checkinUseCase

--- 03-api-solid/use-cases/check-in.ts
+++ 03-api-solid/use-cases/check-in.ts
@@ -18,6 +18,16 @@ export class CheckInUseCase {
     ) {}
 
     async execute({ userId, gymId }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+
+        const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
+            userId,
+            new Date(),
+          )
+
+        if (checkInOnSameDay) {
+            throw new Error()
+        }
+
         const checkIn = await this.checkInsRepository.create({
             user_id: userId,
             gym_id: gymId
