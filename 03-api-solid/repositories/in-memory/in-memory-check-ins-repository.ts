292dd849631d474b904df1d987ba6539f7e48d84
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-04-09 20:11:46 -0300

    feat: create findManyByUserId method

--- 03-api-solid/repositories/in-memory/in-memory-check-ins-repository.ts
+++ 03-api-solid/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -4,6 +4,11 @@ import { randomUUID } from "node:crypto";
 import { CheckInsRepository } from "repositories/check-ins-repositories";
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
+    async findManyByUserId(userId: string, page: number) {
+        return this.items
+            .filter((checkIn) => checkIn.user_id === userId)
+            .slice((page - 1) * 20, page * 20)
+    }
     public items: CheckIn[] = []
 
     async findByUserIdOnDate(userId: string, date: Date) {
