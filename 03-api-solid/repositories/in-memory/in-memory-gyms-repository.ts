175af480ea2161e857e816a216ebb3c7674cd5d2
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-04-08 23:26:31 -0300

    feat: add gym using create method

--- 03-api-solid/repositories/in-memory/in-memory-gyms-repository.ts
+++ 03-api-solid/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,4 +1,6 @@
-import { Gym } from "@prisma/client";
+import { Gym, Prisma } from "@prisma/client";
+import { Decimal } from "@prisma/client/runtime/library";
+import { randomUUID } from "node:crypto";
 import { GymsRepository } from "repositories/gyms-repository";
 
 export class InMemoryGymsRepository implements GymsRepository {
@@ -11,6 +13,22 @@ export class InMemoryGymsRepository implements GymsRepository {
             return null
         }
 
+        return gym
+    }
+
+    async create(data: Prisma.GymCreateInput) {
+        const gym = {
+            id: randomUUID(),
+            title: data.title,
+            description: data.description ?? null,
+            phone: data.phone ?? null,
+            latitude: new Decimal(data.latitude.toString()),
+            longitude: new Decimal(data.longitude.toString()),
+            created_at: new Date(),
+        }
+    
+        this.items.push(gym)
+    
         return gym
     }
 }
