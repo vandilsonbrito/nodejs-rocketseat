c071881c9e2b11f0093143d58a585d3cb884b58c
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-04-08 23:27:38 -0300

    feat: calculate distance between user and gym and varifiy if distance is bigger than 100m

--- 03-api-solid/use-cases/check-in.ts
+++ 03-api-solid/use-cases/check-in.ts
@@ -2,6 +2,8 @@ import { CheckIn } from "@prisma/client";
 import { CheckInsRepository } from "repositories/check-ins-repositories";
 import { ResourceNotFoundError } from "./errors/resource-not-found";
 import { GymsRepository } from "repositories/gyms-repository";
+import { get } from "http";
+import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-coordinates";
 
 interface CheckInUseCaseRequest {
     userId: string
@@ -20,11 +22,22 @@ export class CheckInUseCase {
         private gymsRepository: GymsRepository
     ) {}
 
-    async execute({ userId, gymId }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    async execute({ userId, gymId, userLatitude, userLongitude }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
         const gym = await this.gymsRepository.findById(gymId)
 
         if (!gym) {
             throw new ResourceNotFoundError()
+        }
+
+        const distance = getDistanceBetweenCoordinates(
+            { latitude: userLatitude, longitude: userLongitude },
+            { latitude: gym.latitude.toNumber(), longitude: gym.longitude.toNumber() }
+        )
+
+        const MAX_DISTANCE_IN_KM = 0.1
+        
+        if(distance > MAX_DISTANCE_IN_KM) {
+            throw new Error()
         }
 
         const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
