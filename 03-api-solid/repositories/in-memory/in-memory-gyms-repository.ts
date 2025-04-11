ac39db220ff937239fb956758d838a243ffb85cf
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-04-11 17:45:23 -0300

    feat: create findManyNearby method

--- 03-api-solid/repositories/in-memory/in-memory-gyms-repository.ts
+++ 03-api-solid/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,15 +1,31 @@
+import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-coordinates";
 import { Gym, Prisma } from "@prisma/client";
 import { Decimal } from "@prisma/client/runtime/library";
 import { randomUUID } from "node:crypto";
-import { GymsRepository } from "repositories/gyms-repository";
+import { FindManyNearbyParams, GymsRepository } from "repositories/gyms-repository";
 
 export class InMemoryGymsRepository implements GymsRepository {
+    public items: Gym[] = []
+
+    async findManyNearby(params: FindManyNearbyParams) {
+        return this.items.filter((item) => {
+            const distance = getDistanceBetweenCoordinates(
+                { latitude: params.latitude, longitude: params.longitude },
+                {
+                    latitude: item.latitude.toNumber(),
+                    longitude: item.longitude.toNumber(),
+                }
+            )
+
+            return distance < 10
+        })
+
+    }
     async searchMany(query: string, page: number) {
         return this.items.filter((item) => item.title
             .includes(query))
             .slice((page - 1) * 20, page * 20)
     }
-    public items: Gym[] = []
 
     async findById(id: string) {
         const gym = this.items.find(item => item.id === id)
