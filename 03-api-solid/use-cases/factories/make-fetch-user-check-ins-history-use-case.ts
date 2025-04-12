840e24d0a64827d0d4e02a4f4df74f5d5068fe0c
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-04-12 17:36:23 -0300

    feat: create makeFetchUserCheckInsHistoryUseCase factory

---
+++ 03-api-solid/use-cases/factories/make-fetch-user-check-ins-history-use-case.ts
@@ -0,0 +1,9 @@
+import { PrismaCheckInsRepository } from "repositories/prisma/prisma-check-ins-repository"
+import { FetchUserCheckinHistoryUseCase } from "use-cases/fetch-user-check-ins-history"
+
+export function makeFetchUserCheckInsHistoryUseCase() {
+    const checkInsRepository = new PrismaCheckInsRepository
+    const useCase = new FetchUserCheckinHistoryUseCase(checkInsRepository) 
+
+    return useCase
+}
