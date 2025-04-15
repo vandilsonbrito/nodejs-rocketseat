81bc86962de9cc49c7473c63cdebb32b1467a69e
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-04-15 16:33:04 -0300

    refactor: update use-cases import

--- 03-api-solid/src/http/controllers/profile.ts
+++ 03-api-solid/src/http/controllers/profile.ts
@@ -1,10 +1,10 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
-import { makeGetUserProfileUseCase } from 'use-cases/factories/make-get-user-profile-use-case'
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 
 export async function profile (request: FastifyRequest, reply: FastifyReply) {
 
     await request.jwtVerify()
-    
+
     const getUserProfile = makeGetUserProfileUseCase()
 
     const { user } = await getUserProfile.execute({
