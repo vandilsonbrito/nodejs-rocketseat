d6dcc02497a9a93346db968ebaa3d34d93dcc248
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-04-18 10:24:12 -0300

    feat: add refresh token config sending on reply

--- 03-api-solid/src/http/controllers/users/authenticate.ts
+++ 03-api-solid/src/http/controllers/users/authenticate.ts
@@ -4,39 +4,51 @@ import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-
 import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 
 export async function authenticate (request: FastifyRequest, reply: FastifyReply) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6)
+  })
 
-    const authenticateBodySchema = z.object({
-        email: z.string().email(),
-        password: z.string().min(6)
-    })
+  const { email, password } = authenticateBodySchema.parse(request.body)
 
-    const { email, password } = authenticateBodySchema.parse(request.body)
+  try {
+    const authenticateUseCase = makeAuthenticateUseCase()
+    const { user } = await authenticateUseCase.execute({ email, password })
 
-    try {
-        const authenticateUseCase = makeAuthenticateUseCase()
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
 
-        const { user } = await authenticateUseCase.execute({
-            email,
-            password
-        })
+    const refreshToken = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: '7d',
+        },
+      },
+    )
 
-        const token = await reply.jwtSign(
-            {},
-            {
-              sign: {
-                sub: user.id,
-              },
-            },
-          )
-      
-          return reply.status(200).send({
-            token,
-          })
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({
+        token,
+      })
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: error.message })
     }
-    catch (error) {
-        if (error instanceof InvalidCredentialsError) {
-            return reply.status(400).send({ message: error.message })
-        }
-        return reply.status(500).send()
-    }
+    return reply.status(500).send()
+  }
 }
